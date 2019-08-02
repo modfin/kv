c500eadff9bf7308f05856fb9d4490f5220d5a99
Rasmus Holm <rasmus.holm@modularfinance.se>
2019-08-02 10:42:01 +0200

    adding some extras for ints and uint

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-* A key value helper struct
+# KV
+
+A small wrapper around a key value in order to help with parsing of headers and such
+
+
+
+```go 
+
+kv1 := kv.New("a", 123.34)
+kv2 := kv.New("b", "123.34")
+kv3 := kv.New("a", []int{1,2,3,4})
+
+f1, ok := kv1.Float()
+f2 := kv1.FloatOr(-1.1)
+
+str1, ok := kv2.String()
+str2 := kv2.StringOr("default")
+
+s1, ok := kv3.IntSlice()
+s2 := kv3.IntSliceOr([]int{1})
+
+
+```
--- kv.go
+++ kv.go
@@ -127,19 +127,84 @@ func (kv *KV) StringSliceOr(defaultTo []string) []string {
 	return defaultTo
 }
 
-// Uint returns value as a uint64, if possible
-func (kv *KV) Uint() (uint64, bool) {
+// Uint64 returns value as a uint64, if possible
+func (kv *KV) Uint64() (uint64, bool) {
 	return toUint(kv.value)
 }
 
-// UintOr returns value as a uint64, otherwise the provided default
-func (kv *KV) UintOr(def uint64) uint64 {
+// Uint64Or returns value as a uint64, otherwise the provided default
+func (kv *KV) Uint64Or(def uint64) uint64 {
+	i, ok := kv.Uint64()
+	if ok {
+		return i
+	}
+	return def
+}
+
+// Uint32 returns value as a uint32, if possible
+func (kv *KV) Uint32() (uint32, bool) {
+	u, ok := toUint(kv.value)
+	return uint32(u), ok
+}
+
+// Uint32Or returns value as a uint32, otherwise the provided default
+func (kv *KV) Uint32Or(def uint32) uint32 {
+	i, ok := kv.Uint32()
+	if ok {
+		return i
+	}
+	return def
+}
+
+// Uint16 returns value as a uint16, if possible
+func (kv *KV) Uint16() (uint16, bool) {
+	u, ok := toUint(kv.value)
+	return uint16(u), ok
+}
+
+// Uint16Or returns value as a uint16, otherwise the provided default
+func (kv *KV) Uint16Or(def uint16) uint16 {
+	i, ok := kv.Uint16()
+	if ok {
+		return i
+	}
+	return def
+}
+
+
+// Uint8 returns value as a uint8, if possible
+func (kv *KV) Uint8() (uint8, bool) {
+	u, ok := toUint(kv.value)
+	return uint8(u), ok
+}
+
+
+// Uint8Or returns value as a uint8, otherwise the provided default
+func (kv *KV) Uint8Or(def uint8) uint8 {
+	i, ok := kv.Uint8()
+	if ok {
+		return i
+	}
+	return def
+}
+
+
+// Uint returns value as a uint, if possible
+func (kv *KV) Uint() (uint, bool) {
+	u, ok := toUint(kv.value)
+	return uint(u), ok
+}
+
+
+// UintOr returns value as a uint, otherwise the provided default
+func (kv *KV) UintOr(def uint) uint {
 	i, ok := kv.Uint()
 	if ok {
 		return i
 	}
 	return def
 }
+
 
 // UintSlice returns value as a []uint64, if possible
 func (kv *KV) UintSlice() ([]uint64, bool) {
@@ -173,18 +238,66 @@ func (kv *KV) UintSliceOr(def []uint64) []uint64 {
 	return def
 }
 
-// Int returns value as a int64, if possible
-func (kv *KV) Int() (int64, bool) {
+// Int64 returns value as a int64, if possible
+func (kv *KV) Int64() (int64, bool) {
 	return toInt(kv.value)
 }
 
-// IntOr returns value as a int64, otherwise the provided default
-func (kv *KV) IntOr(def int64) int64 {
-	i, ok := kv.Int()
+// Int64Or returns value as a int64, otherwise the provided default
+func (kv *KV) Int64Or(def int64) int64 {
+	i, ok := kv.Int64()
 	if ok {
 		return i
 	}
 	return def
+}
+
+// Int returns value as a int, if possible
+func (kv *KV) Int() (int, bool) {
+	i, ok := kv.Int64()
+	return int(i), ok
+}
+
+// IntOr returns value as a int, otherwise the provided default
+func (kv *KV) IntOr(def int) int {
+	i := kv.Int64Or(int64(def))
+	return int(i)
+}
+
+// Int32 returns value as a int, if possible
+func (kv *KV) Int32() (int32, bool) {
+	i, ok := kv.Int64()
+	return int32(i), ok
+}
+
+// Int32Or returns value as a int, otherwise the provided default
+func (kv *KV) Int32Or(def int32) int32 {
+	i := kv.Int64Or(int64(def))
+	return int32(i)
+}
+
+// Int16 returns value as a int16, if possible
+func (kv *KV) Int16() (int16, bool) {
+	i, ok := kv.Int64()
+	return int16(i), ok
+}
+
+// Int32Or returns value as a int, otherwise the provided default
+func (kv *KV) Int16Or(def int16) int16 {
+	i := kv.Int64Or(int64(def))
+	return int16(i)
+}
+
+// Int8 returns value as a int8, if possible
+func (kv *KV) Int8() (int8, bool) {
+	i, ok := kv.Int64()
+	return int8(i), ok
+}
+
+// Int8Or returns value as a int, otherwise the provided default
+func (kv *KV) Int8Or(def int8) int8 {
+	i := kv.Int64Or(int64(def))
+	return int8(i)
 }
 
 // IntSlice returns value as a []int64, if possible
